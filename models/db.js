ecb20421d8868a2cbe5826e20a6d706a44404087
azharul <gugly2009@gmail.com>
2018-05-29 15:47:48 +0600

    auth

--- models/db.js
+++ models/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connect to MongoDB on localhost:27017
-//mongoose.connect('mongodb://localhost:27017/book', { useMongoClient: true });
-mongoose.connect('mongodb://emily:emily@ds237660.mlab.com:37660/emily-prod');
+mongoose.connect('mongodb://localhost:27017/book', { useMongoClient: true });
+//mongoose.connect('mongodb://emily:emily@ds237660.mlab.com:37660/emily-prod');
 
 // Create a model and insert a new doc
 // const Test = mongoose.model('Test', new mongoose.Schema({ name: String }));
