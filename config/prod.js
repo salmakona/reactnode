5ea37a72552785fee0e26c034d0467f1d7577142
azharul <gugly2009@gmail.com>
2018-05-30 09:46:22 +0600

    test

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,14 @@
 //prodction keys here 
-module.exports = {
-  googleClientID:process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:process.env.CALLBACK_URL,
-  mongoURI:process.env.MONGO_URI,
-  cookieKey:process.env.COOKIE_KEY,
-};
+// module.exports = {
+//   googleClientID:process.env.GOOGLE_CLIENT_ID,
+//   googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL:process.env.CALLBACK_URL,
+//   mongoURI:process.env.MONGO_URI,
+//   cookieKey:process.env.COOKIE_KEY,
+// };
+
+
+
 
 
   // googleClientID:'1035094270638-t9mtu5gl4vt1s782qtbqb4n1r5t9jfl4.apps.googleusercontent.com',
@@ -13,3 +16,18 @@ module.exports = {
   // callbackURL: 'https://codelover.herokuapp.com/auth/google/callback',
   // mongoURI:'mongodb://emily:emily@ds237660.mlab.com:37660/emily-prod',
   // cookieKey:'jgfjdghjdg',
+
+
+
+
+
+// for deveoplemt mode 
+
+module.exports = {
+  googleClientID:
+    '460490851457-a6gdeadq3e5did54sgr1a8pqh0f80dqt.apps.googleusercontent.com',
+  googleClientSecret: 'tnWSJ3K53jEMKSt2xdXnDWkO',
+  callbackURL: 'http://127.0.0.1:8080/auth/google/callback',
+  mongoURI: 'mongodb://gugly:gugly@ds121960.mlab.com:21960/reactnode',
+  cookieKey:'jgfjdghjdg',
+};
